faca180832f80ca16a4cafefc4fabba48d2ea147
kajalborlepwar <kajal@hubcom.in>
2025-05-20 06:00:24 +0530

    ordinal num

--- src/01_01/ordinal_solution.c
+++ src/01_01/ordinal_solution.c
@@ -1,37 +1,30 @@
-#include <stdio.h>
+// Online C++ compiler to run C++ program online
+#include <iostream>
 
-char *ordinal(int v)
+std::string ordinalnum(int v)
 {
-	/* catch exceptions */
-	if( v==11 || v==12 || v==13 )
-		return("th");
+	if (v == 11 || v == 12 || v == 13 || v == 14)
+		return "th";
 
-	/* everything else is consistent */
-	v %= 10;
-	if( v==1 )
-		return("st");
-	if( v==2 )
-		return("nd");
-	if( v==3 )
-		return("rd");
-	return("th");
+	if (v % 10 == 1)
+		return "st";
+	if (v % 10 == 2)
+		return "nd";
+	if (v % 10 == 3)
+		return "rd";
+	if (v % 10 == 4)
+		return "th";
+
+	return "th";
 }
-
 int main()
 {
-	int c;
-
-	/* output a table of 100 numbers */
-	for( c=1; c<=20; c++ )
+	// Write C++ code here
+	std::cout << "printing table of 20 for 100 numbers" << std::endl;
+	for (int c = 1; c <= 20; c++)
 	{
-		printf("%3d%s\t%3d%s\t%3d%s\t%3d%s\t%3d%s\n",
-				c, ordinal(c),
-				c+20, ordinal(c+20),
-				c+40, ordinal(c+40),
-				c+60, ordinal(c+60),
-				c+80, ordinal(c+80)
-			  );
+		std::cout << "" << c << ordinalnum(c) << " " << c + 20 << ordinalnum(c + 20) << " " << c + 40 << ordinalnum(c + 40) << " " << c + 60 << ordinalnum(c + 60) << " " << c + 80 << ordinalnum(c + 80) << std::endl;
 	}
 
-	return(0);
+	return 0;
 }
